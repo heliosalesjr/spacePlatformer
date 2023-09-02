0d193bbda7c6a443fdbd787f9d75c7346d8c237a
heliosalesjr <heliosalesjr@gmail.com>
2023-09-02 20:39:55 -0300

    all the basic player movements and animations added

--- scripts/level.gd
+++ scripts/level.gd
@@ -1,7 +1,15 @@
 extends Node2D
 
-func _process(delta):
+@onready var start_position = $StartPosition
+
+func _process(_delta):
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
 	elif Input.is_action_just_pressed("reset"):
 		get_tree().reload_current_scene()
+
+
+func _on_death_zone_body_entered(body):
+	body.velocity = Vector2.ZERO
+	body.global_position = start_position.global_position
+	
