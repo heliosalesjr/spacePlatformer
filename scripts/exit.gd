bf75875076e888d6ddf2a27ae4d8512746d96184
heliosalesjr <heliosalesjr@gmail.com>
2023-09-12 09:59:06 -0300

    changing scenes mechanics

--- scripts/exit.gd
+++ scripts/exit.gd
@@ -2,8 +2,8 @@ extends Area2D
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
 
-func _ready():
-	animate()
+#func _ready():
+#	animate()
 
 func animate():
 	animated_sprite_2d.play("default")
--- scripts/level.gd
+++ scripts/level.gd
@@ -1,6 +1,11 @@
 extends Node2D
 
+@export var next_level: PackedScene = null
+
 @onready var start = $Start
+@onready var exit = $Exit
+
+
 var player = null
 
 func _ready():
@@ -10,7 +15,7 @@ func _ready():
 	for trap in traps:
 		trap.connect("touched_player", _on_trap_touched_player)
 		# uma outra opcao pra fazer o mesmo que o acima eh: trap.touched_player.connect(_on_trap_touched_player)
-
+	exit.body_entered.connect(_on_exit_body_entered)
 func _process(_delta):
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
@@ -27,3 +32,11 @@ func _on_trap_touched_player():
 func reset_player():
 	player.velocity = Vector2.ZERO
 	player.global_position = start.get_spawn_position()
+	
+func _on_exit_body_entered(body):
+	if body is Player:
+		if next_level != null:
+			exit.animate()
+			player.active = false
+			await get_tree().create_timer(2).timeout
+			get_tree().change_scene_to_packed(next_level)
