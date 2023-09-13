51916bfdd5b0b924f94a0f665091b1451bb56ef3
heliosalesjr <heliosalesjr@gmail.com>
2023-09-13 11:48:09 -0300

    the win screen

--- scripts/level.gd
+++ scripts/level.gd
@@ -1,10 +1,19 @@
 extends Node2D
 
 @export var next_level: PackedScene = null
-
+@export var is_final_level: bool = false
 @onready var start = $Start
 @onready var exit = $Exit
 @onready var death_zone = $DeathZone
+
+@onready var hud = $UILayer/HUD
+@onready var ui_layer = $UILayer
+@export var level_time = 5
+
+var timer_node = null
+var time_left
+
+var win = false
 
 
 var player = null
@@ -18,6 +27,24 @@ func _ready():
 		# uma outra opcao pra fazer o mesmo que o acima eh: trap.touched_player.connect(_on_trap_touched_player)
 	exit.body_entered.connect(_on_exit_body_entered)
 	death_zone.body_entered.connect(_on_death_zone_body_entered)
+	
+	time_left = level_time
+	hud.set_time_label(time_left)
+	timer_node = Timer.new()
+	timer_node.name = "Level Timer"
+	timer_node.wait_time = 1
+	timer_node.timeout.connect(_on_level_timer_timeout)
+	add_child(timer_node)
+	timer_node.start()
+
+func _on_level_timer_timeout():
+	if win == false:
+		time_left -= 1
+		hud.set_time_label(time_left)
+		if time_left < 0:
+			reset_player()
+			time_left = level_time
+			hud.set_time_label(time_left)
 
 func _process(_delta):
 	if Input.is_action_just_pressed("quit"):
@@ -38,8 +65,12 @@ func reset_player():
 	
 func _on_exit_body_entered(body):
 	if body is Player:
-		if next_level != null:
+		if is_final_level || (next_level != null):
 			exit.animate()
 			player.active = false
+			win = true
 			await get_tree().create_timer(2).timeout
-			get_tree().change_scene_to_packed(next_level)
+			if is_final_level:
+				ui_layer.show_win_screen(true)
+			else:
+				get_tree().change_scene_to_packed(next_level)
